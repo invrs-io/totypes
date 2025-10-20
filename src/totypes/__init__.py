37a0d9a3b7b4344bfc9467fda71c081b8f7058f0
Martin Schubert <martinschubert@Martins-MacBook-Pro.local>
2025-10-20 12:48:13 -0700

    Version updated from v0.9.0 to v0.9.1

--- src/totypes/__init__.py
+++ src/totypes/__init__.py
@@ -3,7 +3,7 @@
 Copyright (c) 2025 invrs.io LLC
 """
 
-__version__ = "v0.9.0"
+__version__ = "v0.9.1"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 __all__ = ["json_utils", "symmetry", "types"]
